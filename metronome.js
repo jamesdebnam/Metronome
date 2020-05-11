57bbf36b63d1e3ede9c677918d3890cc45aca117
James Debnam <jamesdebnam@gmail.com>
2020-05-11 12:42:38 +0100

    Clicking is working, need to add set bpm functionality

--- metronome.js
+++ metronome.js
@@ -1,18 +1,34 @@
 const button = document.querySelector("button");
+const bpmInput = document.querySelector("input");
 let interval = null;
 let isPlaying = false;
 const click = new Audio("tick.mp3");
+let cycleLength = 1000;
+
+const bpmToCycle = (num) => 60000 / num;
+const playClick = () => click.play();
 
 button.addEventListener("click", toggleMetronome);
+
+//this needs sorting
+bpmInput.addEventListener("input", (e) => {
+  console.log(e);
+});
 
 function toggleMetronome() {
   if (isPlaying) {
     isPlaying = false;
+    button.innerHTML = "Start metronome";
+    setPulse();
     clearInterval(interval);
   } else {
     isPlaying = true;
-    interval = setInterval(playclick, 50);
+    button.innerHTML = "Stop metronome";
+    setPulse();
+    interval = setInterval(playClick, cycleLength);
   }
 }
 
-const playclick = () => click.play();
+function setPulse() {
+  // set a pulsating animation for the inner svg circle
+}
